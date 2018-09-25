51667afe508437379b36f87998abc7b502d2abce
yamasita <yamasita@yaasita.net>
2018-09-25 21:03:57 +0900

    update

--- legacy.js
+++ legacy.js
@@ -7,7 +7,7 @@ const common = require("./common.js");
   const messages = await common.get_messages(
     "in/main.db",
     `
-    select from_dispname as name,timestamp as time,body_xml as text from Messages order by timestamp;
+    select from_dispname as name,timestamp as time,body_xml as text,  convo_id as chat from Messages order by timestamp;
     `
   );
   fs.writeFileSync("out/main.json", JSON.stringify(messages, null, 4));
--- skype2json.js
+++ skype2json.js
@@ -7,7 +7,7 @@ const common = require("./common.js");
   const messages = await common.get_messages(
     "in/skype.db",
     `
-    select (m.id/1000) as time,(coalesce(c.display_name,m.author)) as name,m.content as text 
+    select (m.id/1000) as time,(coalesce(c.display_name,m.author)) as name,m.content as text, convdbid as chat
     from messages as m left join contacts as c on m.author = c.mri order by id;
     `
   );
