9abe75b978d1d0a4fbdf36a6c1bb5a86a2332005
yamasita <yamasita@yaasita.net>
2018-11-29 02:18:56 +0900

    update

--- skype2json_v2018.js
+++ skype2json_v2018.js
@@ -7,13 +7,11 @@ const common = require("./lib/2018-common.js");
 (async () => {
   const infile = process.argv[2];
   const outfile = "out/" + path.basename(infile).replace(/\.db$/, "") + ".json";
-  console.log(outfile);
-  //const messages = await common.get_messages(
-  //  infile,
-  //  `
-  //  select (m.id/1000) as time,(coalesce(c.display_name,m.author)) as name,m.content as text, convdbid as chat
-  //  from messages as m left join contacts as c on m.author = c.mri order by id;
-  //  `
-  //);
-  //fs.writeFileSync(outfile, JSON.stringify(messages, null, 4));
+  const messages = await common.get_messages(
+    infile,
+    `
+    select nsp_pk, nsp_data  from messagesv12 limit 100;
+    `
+  );
+  fs.writeFileSync(outfile, JSON.stringify(messages, null, 4));
 })();
