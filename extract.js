c634083fe70cad2685ec947a7d60385ac8730d2e
yamasita <yamasita@yaasita.net>
2018-12-13 01:36:33 +0900

    update

--- extract.js
+++ extract.js
@@ -1,6 +1,27 @@
 "use strict";
 
+const path = require("path");
+const fs = require('fs');
+
 (async () => {
   const infile = process.argv[2];
+  const json = require("./out/s4l-yaasita.json");
+  const target = 2018;
+  let newjson = [];
+  if (infile === undefined){
+    usage();
+    throw new Error("定義されてないよ");
+  }
+  for(let i = 0;i < json.length;i++){
+    const date = new Date(json[i].time*1000);
+    if (date.getFullYear() === target){
+      newjson.push(json[i]);
+    }
+  }
+  fs.writeFileSync(`out/${target}.json`, JSON.stringify(newjson, null, 2));
 })();
 
+function usage() {
+  const nodecmd = path.basename(process.argv[0]);
+  console.log(`usage: ${nodecmd} in/s4l-yaasita.db`);
+}
